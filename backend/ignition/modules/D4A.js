b6eb3c6bf68f24675705dbc3f054c38581c484d7
marceau <marceaule06@gmail.com>
2025-03-30 20:49:54 +0200

    Update ignition script

--- backend/ignition/modules/D4A.js
+++ backend/ignition/modules/D4A.js
@@ -7,10 +7,12 @@ require('dotenv').config();
 const poolAddress = process.env.AAVE_POOL_ADDRESS_SEPOLIA; 
 const usdcTokenAddress = process.env.USDC_ADDRESS_SEPOLIA;
 const sepoliaOwnerAddress = process.env.SEPOLIA_OWNER_ADDRESS;
+const ausdcTokenAddress = process.env.AUSDC_ADDRESS_SEPOLIA;
+const uniswapV2Router02Address = process.env.UNISWAP_ROUTER_ADDRESS_SEPOLIA;
 
 module.exports = buildModule("D4AModule", (m) => {
 
-    const d4A = m.contract("D4A", [sepoliaOwnerAddress, poolAddress, usdcTokenAddress]);
+    const d4A = m.contract("D4A", [sepoliaOwnerAddress, poolAddress, usdcTokenAddress, ausdcTokenAddress, uniswapV2Router02Address]);
 
     return { d4A }
 });
